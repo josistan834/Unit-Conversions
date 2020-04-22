4ce95d8b04509ef0931719827a8609f37a6d3dbc
Josiah Stanley <josistan834@ed.amdsb.ca>
2020-04-22 16:06:32 -0400

    Add project files.

---
+++ Unit Conversions/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace Unit_Conversions
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.option1 = new System.Windows.Forms.RadioButton();
+            this.option2 = new System.Windows.Forms.RadioButton();
+            this.option3 = new System.Windows.Forms.RadioButton();
+            this.option4 = new System.Windows.Forms.RadioButton();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label6 = new System.Windows.Forms.Label();
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.outputBox = new System.Windows.Forms.Label();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(376, 47);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1. Inches to Centimeters";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Palatino Linotype", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(376, 47);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "2. Feet to Centimeters";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Palatino Linotype", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(376, 47);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "3. Yards to Meters";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Palatino Linotype", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 214);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(376, 47);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "4. Miles to Kilometers";
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Palatino Linotype", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 301);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(366, 47);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Enter conversion option number:";
+            // 
+            // option1
+            // 
+            this.option1.AutoSize = true;
+            this.option1.Checked = true;
+            this.option1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option1.Location = new System.Drawing.Point(20, 352);
+            this.option1.Name = "option1";
+            this.option1.Size = new System.Drawing.Size(42, 29);
+            this.option1.TabIndex = 5;
+            this.option1.TabStop = true;
+            this.option1.Text = "1";
+            this.option1.UseVisualStyleBackColor = true;
+            // 
+            // option2
+            // 
+            this.option2.AutoSize = true;
+            this.option2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option2.Location = new System.Drawing.Point(115, 351);
+            this.option2.Name = "option2";
+            this.option2.Size = new System.Drawing.Size(42, 29);
+            this.option2.TabIndex = 6;
+            this.option2.TabStop = true;
+            this.option2.Text = "2";
+            this.option2.UseVisualStyleBackColor = true;
+            // 
+            // option3
+            // 
+            this.option3.AutoSize = true;
+            this.option3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option3.Location = new System.Drawing.Point(220, 351);
+            this.option3.Name = "option3";
+            this.option3.Size = new System.Drawing.Size(42, 29);
+            this.option3.TabIndex = 7;
+            this.option3.TabStop = true;
+            this.option3.Text = "3";
+            this.option3.UseVisualStyleBackColor = true;
+            // 
+            // option4
+            // 
+            this.option4.AutoSize = true;
+            this.option4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option4.Location = new System.Drawing.Point(315, 351);
+            this.option4.Name = "option4";
+            this.option4.Size = new System.Drawing.Size(42, 29);
+            this.option4.TabIndex = 8;
+            this.option4.TabStop = true;
+            this.option4.Text = "4";
+            this.option4.UseVisualStyleBackColor = true;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.ForeColor = System.Drawing.Color.LightSkyBlue;
+            this.progressBar1.Location = new System.Drawing.Point(384, -4);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(47, 442);
+            this.progressBar1.TabIndex = 9;
+            this.progressBar1.Value = 99;
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Palatino Linotype", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(458, 18);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(317, 47);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Enter Value to be Converted";
+            // 
+            // inputBox
+            // 
+            this.inputBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputBox.Location = new System.Drawing.Point(560, 68);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(100, 26);
+            this.inputBox.TabIndex = 11;
+            this.inputBox.Text = "0";
+            this.inputBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // outputBox
+            // 
+            this.outputBox.Font = new System.Drawing.Font("Palatino Linotype", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputBox.Location = new System.Drawing.Point(458, 231);
+            this.outputBox.Name = "outputBox";
+            this.outputBox.Size = new System.Drawing.Size(309, 149);
+            this.outputBox.TabIndex = 12;
+            this.outputBox.Text = "0 Inches = 0 Centimeters";
+            this.outputBox.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // convertButton
+            // 
+            this.convertButton.BackColor = System.Drawing.Color.LightBlue;
+            this.convertButton.Font = new System.Drawing.Font("MV Boli", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.convertButton.Location = new System.Drawing.Point(495, 110);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(228, 88);
+            this.convertButton.TabIndex = 13;
+            this.convertButton.Text = "Convert!";
+            this.convertButton.UseVisualStyleBackColor = false;
+            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 394);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.outputBox);
+            this.Controls.Add(this.inputBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.option4);
+            this.Controls.Add(this.option3);
+            this.Controls.Add(this.option2);
+            this.Controls.Add(this.option1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Unit Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton option1;
+        private System.Windows.Forms.RadioButton option2;
+        private System.Windows.Forms.RadioButton option3;
+        private System.Windows.Forms.RadioButton option4;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.Label outputBox;
+        private System.Windows.Forms.Button convertButton;
+    }
+}
+
---
+++ Unit Conversions/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Unit_Conversions
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void convertButton_Click(object sender, EventArgs e)
+        {
+            double num1 = 0;
+            try
+            {
+                num1 = Convert.ToDouble(inputBox.Text);
+            }
+            catch
+            {
+                outputBox.Text = "Please enter a valid number";
+            }
+
+            if (option1.Checked)
+            {
+                double num2 = ITC(num1);
+                outputBox.Text = $"{num1} Inches = {num2} Centimeters";
+            }
+            else if (option2.Checked)
+            {
+                double num2 = FTC(num1);
+                outputBox.Text = $"{num1} Feet = {num2} Centimeters";
+            }
+            else if (option3.Checked)
+            {
+                double num2 = YTM(num1);
+                outputBox.Text = $"{num1} Yards = {num2} Meters";
+            }
+            else if (option4.Checked)
+            {
+                double num2 = MTK(num1);
+                outputBox.Text = $"{num1} Miles = {num2} Kilometers";
+            }
+        }
+        public static double ITC(double num1)
+        {
+            double output = num1 * 2.54;
+            return output;
+        }
+        public static double FTC(double num1)
+        {
+            double output = num1 * 30.48;
+            return output;
+        }
+        public static double YTM(double num1)
+        {
+            double output = num1 * 0.91;
+            return output;
+        }
+        public static double MTK(double num1)
+        {
+            double output = num1 * 1.6;
+            return output;
+        }
+    }
+}
